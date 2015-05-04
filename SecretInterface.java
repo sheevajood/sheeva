320e1a35aa0f09000ee97d45d857432a9c54f885
sheevajood <sheevajood@gmail.com>
2015-05-04 15:12:39 -0400

    Create SecretInterface.java

---
+++ SecretInterface.java
@@ -0,0 +1,18 @@
+package com.secret;
+
+/**
+ * 
+ * @author xxx
+ *
+ */
+public interface SecretInterface
+{
+
+	/**
+	 * 
+	 * @param num
+	 * @return
+	 */
+   public Integer secret(int num);
+
+}
